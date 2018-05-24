69b11bd34135f02c5ba9605eb6532e49b2ede907
Kyle Damken <kyle_damken@epam.com>
2018-05-24 09:34:04 -0400

    1.0.6 Remove console.logs in content.js

--- js/content.js
+++ js/content.js
@@ -1,10 +1,7 @@
-console.log('content script running')
-
-var console = {};
-console.log = function () {};
+// console.log('content script running')
 
 chrome.extension.onMessage.addListener(function (request, sender, response) {
-    console.log('content script message received')
+    // console.log('content script message received')
     if (request.type === 'turnOnGray') {
         // console.log('turn on gray');
         document.querySelector('html').style.filter = "grayscale(100%)";
@@ -17,17 +14,17 @@ chrome.extension.onMessage.addListener(function (request, sender, response) {
                 if (html && !html.classList.contains('gs-modified-background')) {
                     htmlBackgroundColor = window.getComputedStyle(html, null).getPropertyValue('background-color');
                     htmlBackgroundImage = window.getComputedStyle(html, null).getPropertyValue('background-image');
-                    console.log('htmlBackgroundColor', htmlBackgroundColor);
-                    console.log('htmlBackgroundImage', htmlBackgroundImage);
-                    console.log('tinycolor htmlColor alpha', tinycolor(htmlBackgroundColor).getAlpha());
+                    // console.log('htmlBackgroundColor', htmlBackgroundColor);
+                    // console.log('htmlBackgroundImage', htmlBackgroundImage);
+                    // console.log('tinycolor htmlColor alpha', tinycolor(htmlBackgroundColor).getAlpha());
 
                     if (tinycolor(htmlBackgroundColor).getAlpha() === 0 && htmlBackgroundImage === "none") {
-                        console.log('html is actually transparent and has no image, do nothing');
+                        // console.log('html is actually transparent and has no image, do nothing');
                     } else if (tinycolor(htmlBackgroundColor).getAlpha() < .5 || tinycolor(htmlBackgroundColor).isLight()) {
-                            console.log('html is light, set to white')
+                            // console.log('html is light, set to white')
                             document.querySelector('html').style.background = "#ffffff";
                     } else {
-                        console.log('html is not light, set to gray')
+                        // console.log('html is not light, set to gray')
                         document.querySelector('html').style.background = "#525050";
                     }
 
@@ -39,27 +36,27 @@ chrome.extension.onMessage.addListener(function (request, sender, response) {
                         if (body) {
                             bodyBackgroundColor = window.getComputedStyle(body, null).getPropertyValue('background-color');
                             bodyBackgroundImage = window.getComputedStyle(body, null).getPropertyValue('background-image');
-                            console.log('bodyBackgroundColor', bodyBackgroundColor);
-                            console.log('bodyBackgroundImage', bodyBackgroundImage);
-                            console.log('tinycolor bodyColor alpha', tinycolor(bodyBackgroundColor).getAlpha());
+                            // console.log('bodyBackgroundColor', bodyBackgroundColor);
+                            // console.log('bodyBackgroundImage', bodyBackgroundImage);
+                            // console.log('tinycolor bodyColor alpha', tinycolor(bodyBackgroundColor).getAlpha());
 
                             if (tinycolor(bodyBackgroundColor).getAlpha() === 0 && bodyBackgroundImage === "none") {
-                                console.log('body is actually transparent and has no image, do nothing');
+                                // console.log('body is actually transparent and has no image, do nothing');
                             } else if (tinycolor(bodyBackgroundColor).getAlpha() < .5 || tinycolor(bodyBackgroundColor).isLight()) {
-                                console.log('body is light, set to white')
+                                // console.log('body is light, set to white')
                                 document.querySelector('body').style.background = "#ffffff";
                             } else {
-                                console.log('body is not light, set to gray')
+                                // console.log('body is not light, set to gray')
                                 document.querySelector('body').style.background = "#525050";
                             }
                         } else {
-                            console.log('no body');
+                            // console.log('no body');
                         }
                     }, 100);
 
                     html.classList.add('gs-modified-background');
                 } else {
-                    console.log('Backgrounds already changed, dont run');
+                    // console.log('Backgrounds already changed, dont run');
                 }
             }
         });        
